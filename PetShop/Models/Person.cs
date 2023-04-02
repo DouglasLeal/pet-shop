8832c4bd1a339adcdd45cfd3db0250c8d4481070
Douglas Leal <douglas.leal@live.com>
2023-04-02 19:49:43 -0300

    Person - classe criada

---
+++ PetShop/Models/Person.cs
@@ -0,0 +1,22 @@
+﻿
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace PetShop.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        [DisplayName("Nome")]
+        public string? Name { get; set; }
+        [DisplayName("Tipo")]
+        public PersonType Type { get; set; }
+    }
+
+    public enum PersonType
+    {
+        [Display(Name = "Cliente")]
+        Customer,
+        Employee
+    }
+}
