bf080b7b26fcee78b89392a24fa95b30aab715fb
Douglas Leal <douglas.leal@live.com>
2023-04-03 18:39:03 -0300

    Animal - Validacao com DataAnnotations e migration criada

--- PetShop/Data/ApplicationDbContext.cs
+++ PetShop/Data/ApplicationDbContext.cs
@@ -1,10 +1,13 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using PetShop.Models;
 
 namespace PetShop.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<Animal> Animals { get; set; }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
--- PetShop/Models/Animal.cs
+++ PetShop/Models/Animal.cs
@@ -5,13 +5,24 @@ namespace PetShop.Models
 {
     public class Animal
     {
+        [Key]
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "{0} é obrigatório")]
         [DisplayName("Nome")]
         public string? Name { get; set; }
-        [DisplayName("Cor")]
+
+        [Required(ErrorMessage = "{0} é obrigatório")]
+        [MinLength(3, ErrorMessage = "{0} precisa ter no mínimo {1} caracteres")]
+        [MaxLength(50, ErrorMessage = "{0} possui o limite de {1} caracteres")]
+        [DisplayName("Cor")]        
         public string? Color { get; set; }
+
+        [MaxLength(1000, ErrorMessage = "{0} possui o limite de {1} caracteres")]
         [DisplayName("Observação")]
         public string? Observation { get; set; }
+
+        [Required(ErrorMessage = "{0} é obrigatório")]
         [DisplayName("Tipo")]
         public AnimalType Type { get; set; }
     }
