ac8b268fbabe43ec5fddcc65b8197918a0016c2b
Douglas Leal <douglas.leal@live.com>
2023-04-03 19:01:04 -0300

    Animal - crud criado com scaffold

---
+++ PetShop/Controllers/AnimalsController.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PetShop.Data;
+using PetShop.Models;
+
+namespace PetShop.Controllers
+{
+    [Route("animais")]
+    public class AnimalsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AnimalsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("")]
+        [HttpGet("index")]        
+        public async Task<IActionResult> Index()
+        {
+              return _context.Animals != null ? 
+                          View(await _context.Animals.ToListAsync()) :
+                          Problem("Entity set 'ApplicationDbContext.Animals'  is null.");
+        }
+
+        [HttpGet("detalhes")]
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Animals == null)
+            {
+                return NotFound();
+            }
+
+            var animal = await _context.Animals
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (animal == null)
+            {
+                return NotFound();
+            }
+
+            return View(animal);
+        }
+
+        [HttpGet("novo")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Animals/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Color,Observation,Type")] Animal animal)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(animal);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(animal);
+        }
+
+        [HttpGet("editar")]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Animals == null)
+            {
+                return NotFound();
+            }
+
+            var animal = await _context.Animals.FindAsync(id);
+            if (animal == null)
+            {
+                return NotFound();
+            }
+            return View(animal);
+        }
+
+        // POST: Animals/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Color,Observation,Type")] Animal animal)
+        {
+            if (id != animal.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(animal);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AnimalExists(animal.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(animal);
+        }
+
+        [HttpGet("excluir")]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Animals == null)
+            {
+                return NotFound();
+            }
+
+            var animal = await _context.Animals
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (animal == null)
+            {
+                return NotFound();
+            }
+
+            return View(animal);
+        }
+
+        // POST: Animals/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Animals == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Animals'  is null.");
+            }
+            var animal = await _context.Animals.FindAsync(id);
+            if (animal != null)
+            {
+                _context.Animals.Remove(animal);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AnimalExists(int id)
+        {
+          return (_context.Animals?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
