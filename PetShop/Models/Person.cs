34833b36a707b337509e7f0b460f77b09e731977
Douglas Leal <douglas.leal@live.com>
2023-04-10 22:34:54 -0300

    View person create modificada

--- PetShop/Models/Person.cs
+++ PetShop/Models/Person.cs
@@ -11,12 +11,12 @@ namespace PetShop.Models
         public int Id { get; set; }
 
         [Required(ErrorMessage = "{0} é obrigatório")]
+        [MinLength(3, ErrorMessage = "{0} precisa ter no mínimo {1} caracteres")]
+        [MaxLength(100, ErrorMessage = "{0} possui o limite de {1} caracteres")]
         [DisplayName("Nome")]
         public string? Name { get; set; }
 
         [Required(ErrorMessage = "{0} é obrigatório")]
-        [MinLength(3, ErrorMessage = "{0} precisa ter no mínimo {1} caracteres")]
-        [MaxLength(100, ErrorMessage = "{0} possui o limite de {1} caracteres")]
         [DisplayName("Tipo")]
         public PersonType Type { get; set; }
     }
