2fec9218b77ab171e58fa3c4e15bb4ee8e7cdafb
Douglas Leal <douglas.leal@live.com>
2023-05-25 17:28:06 -0300

    ProductRepository - criado

--- PetShop/Interfaces/IProductRepository.cs
+++ PetShop/Interfaces/IProductRepository.cs
@@ -2,11 +2,11 @@
 
 namespace PetShop.Interfaces
 {
-    public interface IProductRepository
+    public interface IProductRepository : IDisposable
     {
         Task Create(Product product);
         Task<IList<Product>> List();
-        Task<Product> GetById(int id);
+        Task<Product?> GetById(int id);
         Task Update(Product product);
         Task Delete(int id);
     }
---
+++ PetShop/Repositories/ProductRepository.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PetShop.Data;
+using PetShop.Interfaces;
+using PetShop.Models;
+
+namespace PetShop.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task Create(Product product)
+        {
+            await _context.Product.AddAsync(product);
+            await _context.SaveChangesAsync();
+            return;
+        }
+
+        public async Task<IList<Product>> List()
+        {
+            return await _context.Product.OrderBy(p => p.Name).ToListAsync();
+        }
+
+        public async Task<Product?> GetById(int id)
+        {
+            return await _context.Product.FirstOrDefaultAsync(p => id == p.Id);
+        }
+
+        public async Task Delete(int id)
+        {
+            _context.Remove(id);
+            await _context.SaveChangesAsync();
+            return;
+        }
+
+        public async Task Update(Product product)
+        {
+            _context.Update(product);
+            await _context.SaveChangesAsync();
+        }
+
+        public async void Dispose()
+        {
+            await _context.DisposeAsync();
+        }
+    }
+}
