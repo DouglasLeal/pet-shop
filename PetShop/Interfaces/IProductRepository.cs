cd5b71ef5b40e39c32a3f21019df65c887f122b9
Douglas Leal <douglas.leal@live.com>
2023-05-25 17:15:58 -0300

    IProductRepository - criado

---
+++ PetShop/Interfaces/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using PetShop.Models;
+
+namespace PetShop.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task Create(Product product);
+        Task<IList<Product>> List();
+        Task<Product> GetById(int id);
+        Task Update(Product product);
+        Task Delete(int id);
+    }
+}
