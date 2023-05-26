5440aed23e42728c53ef99b102d2eb8c30ef92b2
Douglas Leal <douglas.leal@live.com>
2023-05-26 14:41:25 -0300

    IPersonRepository - criado

---
+++ PetShop/Interfaces/IPersonRepository.cs
@@ -0,0 +1,13 @@
+﻿using PetShop.Models;
+
+namespace PetShop.Interfaces
+{
+    public interface IPersonRepository : IDisposable
+    {
+        Task Create(Person person);
+        Task<IList<Person>> List();
+        Task<Person?> GetById(int id);
+        Task Update(Person person);
+        Task Delete(Person person);
+    }
+}
