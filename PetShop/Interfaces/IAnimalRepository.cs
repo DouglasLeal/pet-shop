a4b713b09eb845bc7c909a87b32c988efc845990
Douglas Leal <douglas.leal@live.com>
2023-05-25 21:44:09 -0300

    IAnimalRepository - criado

---
+++ PetShop/Interfaces/IAnimalRepository.cs
@@ -0,0 +1,13 @@
+﻿using PetShop.Models;
+
+namespace PetShop.Interfaces
+{
+    public interface IAnimalRepository : IDisposable
+    {
+        Task Create(Animal animal);
+        Task<IList<Animal>> List();
+        Task<Animal?> GetById(int id);
+        Task Update(Animal animal);
+        Task Delete(Animal animal);
+    }
+}
