2435280adf5099137d20bb5c5acf5ed0dc95e78d
Douglas Leal <douglas.leal@live.com>
2023-05-26 14:44:56 -0300

    PersonRepository - criado

---
+++ PetShop/Repositories/PersonRepository.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PetShop.Data;
+using PetShop.Interfaces;
+using PetShop.Models;
+
+namespace PetShop.Repositories
+{
+    public class PersonRepository : IPersonRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PersonRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task Create(Person person)
+        {
+            await _context.Persons.AddAsync(person);
+            await _context.SaveChangesAsync();
+            return;
+        }
+
+        public async Task<IList<Person>> List()
+        {
+            return await _context.Persons.OrderBy(p => p.Name).ToListAsync();
+        }
+
+        public async Task<Person?> GetById(int id)
+        {
+            return await _context.Persons.FirstOrDefaultAsync(p => id == p.Id);
+        }
+
+        public async Task Delete(Person person)
+        {
+            _context.Persons.Remove(person);
+            await _context.SaveChangesAsync();
+            return;
+        }
+
+        public async Task Update(Person person)
+        {
+            _context.Persons.Update(person);
+            await _context.SaveChangesAsync();
+        }
+
+        public async void Dispose()
+        {
+            await _context.DisposeAsync();
+        }
+    }
+}
